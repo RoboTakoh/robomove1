a83db48c9242e273cc6df62645d461e3f32ee585
RoboTakoh <80988283+RoboTakoh@users.noreply.github.com>
2021-03-21 15:33:13 -0400

    removed show distance because it was slowing the reactions down.

--- main.ts
+++ main.ts
@@ -18,29 +18,24 @@ ChangeLights(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red), K
 basic.forever(function () {
     moveMotorZIP.show()
     distance = Kitronik_Move_Motor.measure()
-    basic.showNumber(distance)
-    if (distance >= 14) {
+    if (distance >= 25) {
         ChangeLights(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 30)
-        basic.pause(2000)
-        Kitronik_Move_Motor.stop()
-    } else if (distance < 10) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
+        distance = Kitronik_Move_Motor.measure()
+    } else if (distance < 25) {
         ChangeLights(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green))
         Kitronik_Move_Motor.stop()
-        basic.pause(500)
+        basic.pause(100)
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 30)
         basic.pause(500)
         Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 30)
         basic.pause(500)
         Kitronik_Move_Motor.stop()
-    } else if (distance <= 10 && distance < 14) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 30)
-        ChangeLights(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
-        basic.pause(500)
-        Kitronik_Move_Motor.stop()
+        distance = Kitronik_Move_Motor.measure()
     } else {
         ChangeLights(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red), Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
         Kitronik_Move_Motor.stop()
+        distance = Kitronik_Move_Motor.measure()
     }
     basic.pause(1)
 })
